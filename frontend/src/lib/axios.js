1b59f09d4f3cefbd1050ccf2e0a563c8b6349c8d
Jyotishwar Raj Shukla <shuklarishabh7790@gmail.com>
2025-11-09 19:53:23 +0530

    Trim baseURL for axios instance configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL:import.meta.env.VITE_BACKEND_API_URL || "http://localhost:4000/api",
+  baseURL:import.meta.env.VITE_BACKEND_API_URL.trim () || "http://localhost:4000/api",
   withCredentials: true,
 });
