c9a9ffc9967d94792e78e5ea2c78ec78d8d02008
Jyotishwar Raj Shukla <shuklarishabh7790@gmail.com>
2025-11-09 19:38:49 +0530

    Fix formatting of FRONTEND_URL assignment

--- chat-backend/src/server.js
+++ chat-backend/src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL|| "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
