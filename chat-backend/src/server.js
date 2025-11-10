c80c42e0784a72abf5f36e1ecb1ec4a165482f6b
Jyotishwar Raj Shukla <shuklarishabh7790@gmail.com>
2025-11-10 17:50:53 +0530

    Change server port from 5000 to 5001

--- chat-backend/src/server.js
+++ chat-backend/src/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`✅ Server running on PORT: ${PORT}`);
