ffde04cd8b6e0458211ec3346742caf9d8192ea2
Jyotishwar Raj Shukla <shuklarishabh7790@gmail.com>
2025-11-10 11:43:05 +0530

    Fix CORS origin configuration

--- chat-backend/src/server.js
+++ chat-backend/src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL.trim () || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
